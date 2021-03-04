2d854111cef20e33ea6bfc14aa626be9eca26863
Velid Duranovic <velid_work@outlook.com>
2021-03-04 23:47:16 +0100

    Added changes on the Card component

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Search placeholder="Custom search"/>
       <Button title="Watch now"/>
       <Button title="CTA Link" url="https://google.com"/>
-      <Card />
+      <Card imageUrl="https://picsum.photos/500/300" title="First Sample Card" description="This is my first samle card. It may contains image, title, description, and tags." tags={["#Advanture", "#Gaming", "#Tag"]}/>
     </div>
   );
 }
--- simple-app/src/components/card/card.jsx
+++ simple-app/src/components/card/card.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import './card.scss';
 
-export const Card = (props)=>{
+export const Card = (props) => {
+    let tags;
+    let description;
+    let title;
+    if(props.tags != null){
+        tags = <div className="tags">{props.tags.map(x=><span className="tag">{x}</span>)}</div>
+    }
+    if(props.description){
+        description = <p>{props.description}</p>
+    }
+    if(props.title){
+        title = <h3>{props.title}</h3>
+    }
     return (
         <div className="vd-card">
             <span className="img-holder"></span>
             <picture>
-                <source srcSet="https://picsum.photos/500/300"/>
-                <source srcSet="https://picsum.photos/500/300"/>
-                <img src="https://picsum.photos/500/300" alt="Alt text for the image"/>
+                <source srcSet={props.imageUrl} />
+                <source srcSet={props.imageUrl} />
+                <img src={props.imageUrl} alt="Alt text for the image" />
             </picture>
             <div className="content">
-            <h3>Some card title</h3>
-            <p>Lorem ipsum dolor sit amet, Lorem20 consectetur adipisicing elit. Aspernatur, delectus?</p>
-            <div className="tags">
-                <span className="tag">#photography</span>
-                <span className="tag">#travel</span>
-                <span className="tag">#winter</span>
-            </div>
+                {title}
+                {description}
+                {tags}
             </div>
         </div>
     )
